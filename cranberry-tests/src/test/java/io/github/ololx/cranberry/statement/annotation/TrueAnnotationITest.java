ec54fe3fffaf4610e3b588c7c257997756618a62
Alexander A. Kropotin <ololx@icloud.com>
2021-12-27 20:48:53 +0500

    feature(*) add true annotations tests

---
+++ cranberry-tests/src/test/java/io/github/ololx/cranberry/statement/annotation/TrueAnnotationITest.java
@@ -0,0 +1,32 @@
+package io.github.ololx.cranberry.statement.annotation;
+
+/**
+ * project cranberry
+ * created 2021-12-27 16:29
+ *
+ * @author Alexander A. Kropotin
+ */
+public abstract class TrueAnnotationITest {
+
+    /**
+     * Execute with annotated param object.
+     *
+     * @param param the param
+     * @return the object
+     */
+    protected Object executeWithAnnotatedParam(@True Boolean param) {
+        return param;
+    }
+
+    /**
+     * Execute with annotated local variable object.
+     *
+     * @param param the param
+     * @return the object
+     */
+    protected Object executeWithAnnotatedLocalVariable(Boolean param) {
+        @True Boolean variable = param;
+
+        return param;
+    }
+}
